60bc14a8d93b9f42a93aa22149990c7718d4630e
Nikita Lipanov <95234944+legendes123@users.noreply.github.com>
2023-12-21 12:03:42 +0300

    123
    123

---
+++ src/js/modal-room.js
@@ -0,0 +1,36 @@
+(() => {
+    let openModalBtn = document.querySelectorAll('[data-modal-open-room]');
+
+    const refs = {
+        closeModalBtn: document.querySelector('[data-modal-close-room]'),
+        modal: document.querySelector('[data-modal-room]'),
+        body: document.querySelector('[data-body]'),
+    };
+
+    openModalBtn.forEach(element => {
+        element.addEventListener('click', toggleModal);
+    });
+
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.modal.addEventListener('click', onBackdropClick);
+    window.addEventListener('keydown', onEscClick);
+
+    function onEscClick(e) {
+        if (e.code === 'Escape') {
+            refs.modal.classList.add('is-hidden');
+            refs.body.classList.remove('no-scroll');
+        }
+    }
+
+    function onBackdropClick(e) {
+        if (e.currentTarget === e.target) {
+            refs.modal.classList.add('is-hidden');
+            refs.body.classList.remove('no-scroll');
+        }
+    }
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+        refs.body.classList.toggle('no-scroll');
+    }
+})();
---
+++ src/js/slick-about.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('.slick-about').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    rows: 1,
+  });
+});
